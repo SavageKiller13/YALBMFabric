ff55d806d5b7ed480520c920131949eb1f94b718
BuildTools <unconfigured@null.spigotmc.org>
2021-01-13 21:06:30 -0700

    Added New Coin with recipe

--- src/main/java/com/savagekiller13/yalbm/registry/ItemRegistry.java
+++ src/main/java/com/savagekiller13/yalbm/registry/ItemRegistry.java
@@ -22,9 +22,9 @@ public class ItemRegistry {
     public static final Item ITEM_SKELETON_SKULL_SHARD = new ItemShard((new Item.Settings()).group(ItemGroup.MISC).maxCount(16));
     public static final Item ITEM_DIAMOND_SHARD = new ItemShard((new Item.Settings()).group(ItemGroup.MATERIALS));
     public static final Item ITEM_BASIC_COIN = new ItemCoin((new Item.Settings()).group(ItemGroup.MISC));
+    public static final Item ITEM_RARE_COIN = new ItemCoin((new Item.Settings()).group(ItemGroup.MISC));
 
-
-    public static final Item ITEM_LUCKY_SWORD = new ItemLuckySword(ToolMaterialLucky.COMMON, 7, -3.9F, (new Item.Settings()).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
+    public static final Item ITEM_LUCKY_SWORD = new ItemLuckySword(ToolMaterialLucky.COMMON, 7, -2.0F, (new Item.Settings()).group(ItemGroup.TOOLS).rarity(Rarity.COMMON));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(modid, "poor_gold_apple"), ITEM_POOR_GOLD_APPLE);
@@ -32,6 +32,7 @@ public class ItemRegistry {
         Registry.register(Registry.ITEM, new Identifier(modid, "skeleton_skull_shard"), ITEM_SKELETON_SKULL_SHARD);
         Registry.register(Registry.ITEM, new Identifier(modid, "diamond_shard"), ITEM_DIAMOND_SHARD);
         Registry.register(Registry.ITEM, new Identifier(modid, "coin"), ITEM_BASIC_COIN);
+        Registry.register(Registry.ITEM, new Identifier(modid, "rare_coin"), ITEM_RARE_COIN);
 
         Registry.register(Registry.ITEM, new Identifier(modid, "lucky_sword_common"), ITEM_LUCKY_SWORD);
     }
