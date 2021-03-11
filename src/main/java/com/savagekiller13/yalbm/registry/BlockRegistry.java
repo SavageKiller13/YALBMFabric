2b48f6fcb11512b40f6ad6f167fe061d0ee512d6
BuildTools <unconfigured@null.spigotmc.org>
2021-03-10 21:32:50 -0700

    Added Rarity Specific Blocks (Starting with Common)

--- src/main/java/com/savagekiller13/yalbm/registry/BlockRegistry.java
+++ src/main/java/com/savagekiller13/yalbm/registry/BlockRegistry.java
@@ -1,6 +1,7 @@
 package com.savagekiller13.yalbm.registry;
 
 import com.savagekiller13.yalbm.YALBMFabric;
+import com.savagekiller13.yalbm.block.CommonLuckyBlock;
 import com.savagekiller13.yalbm.block.LuckyBlock;
 import com.savagekiller13.yalbm.block.MorningWood;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -15,19 +16,20 @@ import net.minecraft.util.registry.Registry;
 
 public class BlockRegistry {
 
-    public static final Block BLOCK_LUCKY = new LuckyBlock(FabricBlockSettings.of(Material.STONE).hardness(0.5f).sounds(BlockSoundGroup.WOOD));
+    public static final Block BLOCK_LUCKY = new LuckyBlock(FabricBlockSettings.of(Material.STONE).hardness(0.5f).sounds(BlockSoundGroup.METAL));
     public static final Block MORNING_WOOD = new MorningWood(FabricBlockSettings.of(Material.WOOD).hardness(0.25f).sounds(BlockSoundGroup.STONE));
-
+    public static final Block COMMON_LUCKY_BLOCK = new CommonLuckyBlock(FabricBlockSettings.of(Material.STONE).hardness(0.5f).sounds(BlockSoundGroup.METAL));
 
     public static void registerBlocks() {
-
          Registry.register(Registry.BLOCK, new Identifier(YALBMFabric.modid, "lucky_block"), BLOCK_LUCKY);
          Registry.register(Registry.BLOCK, new Identifier(YALBMFabric.modid, "morning_wood"), MORNING_WOOD);
+         Registry.register(Registry.BLOCK, new Identifier(YALBMFabric.modid, "common_lucky_block"), COMMON_LUCKY_BLOCK);
     }
 
     public static void registerBlockItems() {
         Registry.register(Registry.ITEM, new Identifier(YALBMFabric.modid, "lucky_block"), new BlockItem(BLOCK_LUCKY, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
         Registry.register(Registry.ITEM, new Identifier(YALBMFabric.modid, "morning_wood"), new BlockItem(MORNING_WOOD, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+        Registry.register(Registry.ITEM, new Identifier(YALBMFabric.modid, "common_lucky_block"), new BlockItem(COMMON_LUCKY_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 
     public static void register() {
